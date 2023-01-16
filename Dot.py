873bd92fa998a4f00d1cc13120d324f3e48be406
AndruGomes13 <79923141+AndruGomes13@users.noreply.github.com>
2023-01-16 12:07:24 +0000

    Added config file (global variable file)

---
+++ Dot.py
@@ -0,0 +1,33 @@
+import config
+import pygame
+
+
+
+class Dot:
+    def __init__(self, x, y, color="black") -> None:
+        self.x = x
+        self.y = y
+        self.radius = 10
+        self._color = color
+        self._color_code = config.color_dict[color]
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+        self._color_code = config.color_dict[value]
+
+    @property
+    def color_code(self):
+        return self._color_code
+
+    @color_code.setter
+    def color_code(self, value):
+        self._color_code = value
+        self._color = f"Custom Color: {self._color_code}"
+
+    def draw(self, win):
+        pygame.draw.circle(win, self._color_code, (self.x, self.y), self.radius)
---
+++ config.py
@@ -0,0 +1,9 @@
+# settings.py
+
+def init():
+    global color_dict
+    color_dict = {"white" : (255,255,255),
+            "black" : (0, 0, 0),
+            "grey" : (190, 190, 190),
+            "red" : (255, 0, 0),
+            }
