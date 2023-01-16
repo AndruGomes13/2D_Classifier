fa162dc3ebecdbd1f4443faa5fbcb5add02ddef9
AndruGomes13 <79923141+AndruGomes13@users.noreply.github.com>
2023-01-16 15:57:05 +0000

    Improved button implementation

---
+++ Button.py
@@ -0,0 +1,70 @@
+import config
+import pygame
+
+class Button:
+    def __init__(self, x, y, w, h, 
+    font_color="black", 
+    font_size = 30,
+    caption ="", 
+    background_color = "grey"):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.limits = ((x, x + w),(y, y + h))
+        self.caption = caption
+
+        self._font_color = font_color
+        self._font_color_code = config.color_dict[font_color]
+        self.font_size = font_size
+
+        self._background_color = background_color
+        self._background_color_code = config.color_dict[background_color]
+
+        self.active = True
+    @property
+    def font_color(self):
+        return self._color
+
+    @font_color.setter
+    def _font_color(self, value):
+        self._color = value
+        self._font_color_code = config.color_dict[value]
+
+    @property
+    def color_code(self):
+        return self._font_color_code
+
+    @color_code.setter
+    def color_code(self, value):
+        self._font_color_code = value
+        self._color = f"Custom Color: {self._font_color_code}"
+
+    def draw(self, win):
+        # Draw button
+        button_rect = pygame.Rect(self.x, self.y, self.w, self.h)
+        pygame.draw.rect(win, self._background_color_code, button_rect)
+        
+        # Draw caption
+        font = pygame.font.Font(None, self.font_size)
+        text = font.render(self.caption, True, (0, 0, 0))
+        rect_text = text.get_rect(center=(self.x + self.w // 2, self.y + self.h // 2))
+        win.blit(text, button_rect)
+        pass
+
+    def check_press(self, mouse_coord):
+        return (
+            mouse_coord[0] > self.limits[0][0] and 
+            mouse_coord[0] < self.limits[0][1] and
+            mouse_coord[1] > self.limits[1][0] and
+            mouse_coord[1] < self.limits[1][1])
+
+
+
+        # if (mouse_coord[0] > self.limits[0][0] and 
+        # mouse_coord[0] < self.limits[0][1] and
+        # mouse_coord[1] > self.limits[1][0] and
+        # mouse_coord[1] > self.limits[1][1]):
+        #     return True
+        # else:
+        #     return False
--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import pygame
 from Dot import *
+from Button import *
 import config
-### Object Creation ###
-
-
-class Button:
-    def __init__(self) -> None:
-        pass
-
 
 ### Variable Declaration ###
 
 # Creating window
 DISPLAY_DIM = (500, 500)
 WIN = pygame.display.set_mode(DISPLAY_DIM)
+pygame.display.set_caption('Show Text')
 
 # Frame-rate set
 FPS = 60
 clock = pygame.time.Clock()
 
 # Initialize global project variables
+pygame.init()
 config.init()
 
 # Dot list
 dot_list = []
+
+button = Button(10, 200, 100, 100, caption="Test", background_color="red", font_size=30)
+
 
 def main():
     # Frame-rate set
@@ -40,14 +39,19 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse = pygame.mouse.get_pos()
                 if event.button == 1:
                     color = "red"
+                    dot = Dot(mouse[0],mouse[1], color)
+                    dot_list.append(dot)
                 if event.button == 3:
                     color = "black"
+                    dot = Dot(mouse[0],mouse[1], color)
+                    dot_list.append(dot)
+                if event.button == 2 and button.check_press(mouse):
+                    print("clicked")
 
-                mouse = pygame.mouse.get_pos()
-                dot = Dot(mouse[0],mouse[1], color)
-                dot_list.append(dot)
+
   
 
 
@@ -56,6 +60,7 @@ def main():
         WIN.fill(config.color_dict["white"])
         for dot in dot_list:
             dot.draw(WIN)
+        button.draw(WIN)
         pygame.display.update()
     
 
